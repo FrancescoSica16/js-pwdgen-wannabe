370847bd299353e49763b6a50b397d305de751d8
FrancescoSica16 <francesco.sica16@gmail.com>
2021-08-04 17:21:35 +0200

    aggiunta n° random in psw

--- myscript.js
+++ myscript.js
@@ -1,10 +1,13 @@
-console.log("prova");
-
 document.getElementById("titol").innerHTML ="generiamo noi la tua password";
 
 var nome = prompt("qual'è il tuo nome?", "inserisci qui il nome");
 var cognome = prompt("qual'è il tuo cognome?");
 var colore = prompt("qual'è il tuo colore preferito?");
 
-var pswGen = nome + cognome + colore + "21"; 
-document.getElementById("pswGen").innerHTML = pswGen;
+// var rand = Math.floor(Math.random() * 11);
+var rand = parseInt (Math.random() * 11) ;
+
+var pswGen = nome + cognome + colore + rand ;
+var contHtre = document.getElementById("psw-gen").innerHTML; 
+document.getElementById("psw-gen").innerHTML = contHtre + " " + pswGen;
+
